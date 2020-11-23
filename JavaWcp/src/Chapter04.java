9307676d02205ff08745d84e07a94b3cab018a88
ayana0120 <keichan0501.ayana@gmail.com>
2020-11-23 23:14:05 +0900

    [chapter4]確認問題

---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,16 @@
+
+public class Chapter04 {
+	public static void main(String[]args) {
+		final int num = 5;
+		String myname;
+		myname = "山田太郎";
+		System.out.println(myname);
+		
+		myname = "神話";
+		System.out.println(myname);
+		
+		System.out.println(num);
+		
+	}
+
+}
