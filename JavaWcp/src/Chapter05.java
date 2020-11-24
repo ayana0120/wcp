6ef9f5fc6b610c713b92552096cadea822a00d74
ayana0120 <keichan0501.ayana@gmail.com>
2020-11-24 13:32:57 +0900

    [chapter5]確認問題

---
+++ JavaWcp/src/Chapter05.java
@@ -0,0 +1,23 @@
+public class Chapter05 {
+	public static void main(String[]args) {
+		long long1 = 1;
+		double double1 = 1.2;
+		boolean boolean1 = true;
+		System.out.println(long1);
+		System.out.println(double1);
+		System.out.println(boolean1);
+
+		Float float1 = 0.2f;
+		Integer integer1 = 3;
+		Character character1 = '4';
+		System.out.println(float1);
+		System.out.println(integer1);
+		System.out.println(character1);
+		
+		int[] array1 = new int[] {10, 20, 30, 40, 50};
+		System.out.println(array1[3]);
+		
+		
+	}
+
+}
