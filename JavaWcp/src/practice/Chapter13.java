e204f6bae3345a1e279ce56f72e2241fa8107b8f
ayana0120 <keichan0501.ayana@gmail.com>
2020-11-27 18:26:12 +0900

    [chapter13]確認問題

--- JavaWcp/src/practice/Chapter13.java
+++ JavaWcp/src/practice/Chapter13.java
@@ -10,7 +10,7 @@ public class Chapter13 {
 		Chapter13.staticCall(mobilePhone, "99-8888-7777");
 		mobilePhone.powerOff();
 		
-		FlyingPhone flyingPhone = new Flyingphone(5);
+		FlyingPhone flyingPhone = new FlyingPhone(5);
 		flyingPhone.fly();
 		Chapter13.staticCall(flyingPhone, "000-1111-2222");
 		flyingPhone.powerOff();
